008fad108e8964059784e5ccdf832786c81c1ebf
alawa2020 <huder13168@gmail.com>
2022-01-02 18:38:56 -0500

    ⏏️ action creators added

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,3 +3,13 @@ import { Expense } from '../interfaces/interfaces';
 export type ExpenseAction =
   | { type: 'add'; payload: Expense }
   | { type: 'delete'; payload: { id: number } };
+
+export const doAdd = (expense: Expense): ExpenseAction => ({
+  type: 'add',
+  payload: expense,
+});
+
+export const doDelete = (id: number): ExpenseAction => ({
+  type: 'delete',
+  payload: { id },
+});
